bc094460360bdf7724667580bc3e7b96f124fc32
GABRIEL LUVIELMO XAVIER <gabriel_l_xavier@estudante.sesisenai.org.br>
2024-10-08 09:52:56 -0300

    Segundo Commit

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ const app = express()
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
+app.get('/peixe', (req,res) => {
+    res.render('peixe')
+})
+
 app.get('/', (req, res) =>{
     const user = {
         name: 'Jair Luiz Messias Inacio Lula Bolsonaro da Silva',
         age: 23,
-        email: 'recebajairlula@hotmail.com'
+        email: 'recebajairlula@hotmail.gov'
     }
-    res.render('neyma', {user: user})
+    const auth = false
+
+    const approved = true
+
+    res.render('neyma', {user: user, auth, approved })
 })
 
 app.listen(3000)
